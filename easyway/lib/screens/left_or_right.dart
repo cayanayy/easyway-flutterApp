3bf8488c8b68ce93d2f4f02efbeab166860dce28
CAYAN JIYAN AY <cayanjiyan62@hotmail.com>
2022-06-11 19:10:33 +0300

    left or right little improvement

--- easyway/lib/screens/left_or_right.dart
+++ easyway/lib/screens/left_or_right.dart
@@ -1,12 +1,54 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class LeftOrRight extends StatelessWidget {
+class LeftOrRight extends StatefulWidget {
   const LeftOrRight({Key? key}) : super(key: key);
 
   @override
+  State<LeftOrRight> createState() => _LeftOrRightState();
+}
+
+class _LeftOrRightState extends State<LeftOrRight> {
+  List _foods = [];
+  int currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    readJson();
+  }
+
+  Future<void> readJson() async {
+    final String response =
+        await rootBundle.loadString('assets/food_images.json');
+    final data = await json.decode(response);
+    setState(() {
+      _foods = data["images"];
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('left or right screen'),
+    return Container(
+      decoration: const BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black,
+            blurRadius: 400.0,
+          )
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          GestureDetector(
+              child: Image.network(_foods[currentIndex]["url"]),
+              onTap: () => {setState(() => currentIndex++)}),
+          OutlinedButton(
+              onPressed: () {}, child: const Text("Show my choices")),
+        ],
+      ),
     );
   }
 }
